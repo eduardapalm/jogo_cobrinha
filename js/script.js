f3cebb6e62325173d45ce91ae0c15e1cce63932d
Eduarda Almeida <eduardapalm27@gmail.com>
2021-03-12 11:49:03 -0300

    Criando função para que a cobrinha permaneça na tela de jogo

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-
 snake[0] = {
     x: 8 * box,
     y: 8 * box
@@ -21,7 +20,22 @@ function criandoCobrinha() {
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+
 function iniciarJogo() {
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criandoCobrinha();
 
@@ -29,9 +43,9 @@ function iniciarJogo() {
     let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
-    if (directions == "left") snakeX -=box;
+    if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
-    if (direction == "down") snakeY += box;
+    if(direction == "down") snakeY += box;
 
     snake.pop();
 
@@ -44,6 +58,3 @@ function iniciarJogo() {
 }
 
 let jogo = setInterval(iniciarJogo, 100);
-
-
-
