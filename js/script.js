ae680f408ad7bd09d1803163b6b3a44ccd0431d1
Eduarda Almeida <eduardapalm27@gmail.com>
2021-03-12 11:59:05 -0300

    Criando a comidinha e acionando math.random

--- js/script.js
+++ js/script.js
@@ -6,7 +6,12 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-let direction = "right"; 
+let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+
+} 
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -15,9 +20,14 @@ function criarBG() {
 
 function criandoCobrinha() {
     for(i=0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "orange";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -38,6 +48,7 @@ function iniciarJogo() {
 
     criarBG();
     criandoCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
