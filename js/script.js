90ffad83e4bbd6468886567d88f3fa00cb93f0c9
Eduarda Almeida <eduardapalm27@gmail.com>
2021-03-12 11:08:37 -0300

    Criando corpo e dando movimento a nossa cobrinha

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right"; 
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -20,6 +21,29 @@ function criandoCobrinha() {
     }
 }
 
-criarBG();
-criandoCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criandoCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if (directions == "left") snakeX -=box;
+    if(direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
+
 
